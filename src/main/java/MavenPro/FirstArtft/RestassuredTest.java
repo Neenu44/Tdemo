19d166a6405fcb144debc36d126be1424cb3c719
Neenu <neenubabychan@gmail.com>
2021-07-18 17:11:58 +0530

    first commit

---
+++ src/main/java/MavenPro/FirstArtft/RestassuredTest.java
@@ -0,0 +1,19 @@
+package MavenPro.FirstArtft;
+
+import org.testng.annotations.Test;
+
+public class RestassuredTest {
+
+	@Test
+	public void Domestic()
+	{
+		System.out.println("Cat RestAssured");
+	}
+	
+	@Test
+	public void wild()
+	{
+		System.out.println("Lion RestAssured");
+	}
+
+}
---
+++ src/main/java/MavenPro/FirstArtft/SeleniumTest.java
@@ -0,0 +1,18 @@
+package MavenPro.FirstArtft;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void Flower1()
+	{
+		System.out.println("Rose RestAssured");
+	}
+	@Test
+	public void Flower2()
+	{
+		System.out.println("Lilly RestAssured");
+	}
+
+}
